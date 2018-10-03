593c97bcf38a75b10a2e4e02ca6d390ce9792e49
S Chua <sean.chua@ga.gov.au>
2018-10-03 15:32:24 +1000

    generalising the spei_stats script for DEA notebooks

--- spei_stats.py
+++ spei_stats.py
@@ -1,13 +1,13 @@
 from datacube_stats import StatsApp
 from datacube_stats.statistics import GeoMedian
 from datacube import Datacube
-import yaml
 from datacube_stats.utils import pickle_stream, unpickle_stream
+import yaml
 import multiprocessing
-
 import xarray as xr
 
 def save_tasks():
+    # Creates a pickle file with the locations of every datacube observation specified in the yaml file 
     with open('stats_config.yaml') as fl:
         config = yaml.load(fl)
 
@@ -17,6 +17,55 @@ def save_tasks():
     dc = Datacube(app='api-example', config='cambodia.conf')
     app = StatsApp(config, index = dc.index)
     pickle_stream(app.generate_tasks(dc.index), 'task.pickle')
+    
+def prune_tasks(quartile):
+    # 'prunes' the output pickle file from save_tasks() with a list of dates to create a new pickle file
+    
+    # Import netcdf with the list of dates
+    spei_q_dates = xr.open_dataset(f"spei_q{quartile}_dates.nc")
+
+    pruned = (transform_task(task, spei_q_dates, quartile)
+              for task in unpickle_stream('task.pickle'))
+    pruned = (task for task in pruned if task is not None)
+
+    pickle_stream(pruned, f'task_q{quartile}.pickle')
+
+def transform_task(task, q_dates, quartile):
+    # fix output file path to include the quartile
+    new_path = 'nbart_geomedian_q' + str(quartile) + '_{x}_{y}.nc'
+    task.output_products['ls_level2_geomedian_annual'].file_path_template = new_path
+
+    task.sources = [transform_source(source, q_dates) for source in task.sources]
+    task.sources = [source for source in task.sources if source is not None]
+
+    if task.sources == []:
+        return None
+
+    return task
+
+def transform_source(source, q_dates):
+    try:
+        source.data = transform_tile(source.data, q_dates)
+        source.masks = [transform_tile(mask, q_dates) for mask in source.masks]
+    except ValueError:
+        return None
+
+    return source
+
+def transform_tile(tile, q_dates):
+    [num_observations] = tile.sources.time.shape
+
+    sources = []
+    
+    #Check if task time is within list of dates
+    for i in range(num_observations):
+        one_slice = tile.sources.isel(time=slice(i, i + 1))
+        if one_slice.time.astype('datetime64[M]') in q_dates.time.values.astype('datetime64[M]'):
+            sources.append(one_slice)
+
+    tile.sources = xr.concat(sources, dim='time')
+
+    return tile
 
 def execute_tasks(quartile):
     with open('stats_config.yaml') as fl:
@@ -32,64 +81,8 @@ def execute_tasks(quartile):
     p = multiprocessing.Pool()
     p.map(app.execute_task, list(unpickle_stream(task_file)))
 
-#    for x in generator:
-#        perform(x)
-#    map(perform, generator)
-#    [perform(x) for x in generator]
-
-#    for task in unpickle_stream(task_file):
-#        print('executing', task)
-#        app.execute_task(task)
-
-def transform_tile(tile, q_dates):
-    [num_observations] = tile.sources.time.shape
-
-    sources = []
-
-    for i in range(num_observations):
-        one_slice = tile.sources.isel(time=slice(i, i + 1))
-        if one_slice.time.astype('datetime64[M]') in q_dates.time.values.astype('datetime64[M]'):
-            sources.append(one_slice)
-
-    tile.sources = xr.concat(sources, dim='time')
-
-    return tile
-
-def transform_source(source, q_dates):
-    try:
-        source.data = transform_tile(source.data, q_dates)
-        source.masks = [transform_tile(mask, q_dates) for mask in source.masks]
-    except ValueError:
-        return None
-
-    return source
-
-def transform_task(task, q_dates, quartile):
-    # fix output file path to include the quartile
-    new_path = 'nbart_geomedian_q' + str(quartile) + '_{x}_{y}.nc'
-    task.output_products['ls_level2_geomedian_annual'].file_path_template = new_path
-
-    # prune by the quartile dates
-    task.sources = [transform_source(source, q_dates) for source in task.sources]
-    task.sources = [source for source in task.sources if source is not None]
-
-    if task.sources == []:
-        return None
-
-    return task
-
-def prune_tasks(quartile):
-    spei_q_dates = xr.open_dataset(f"spei_q{quartile}_dates.nc")
-
-    pruned = (transform_task(task, spei_q_dates, quartile)
-              for task in unpickle_stream('task.pickle'))
-    pruned = (task for task in pruned if task is not None)
-
-    pickle_stream(pruned, f'task_q{quartile}.pickle')
-
 if __name__ == '__main__':
     save_tasks()            
-    for i in [1, 4]:
-        prune_tasks(i)
-        execute_tasks(i)
+    prune_tasks(i)
+    execute_tasks(i)
 
